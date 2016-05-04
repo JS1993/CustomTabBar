1d56b5048194fc0fa569bc5e8bc9202833c5ba00
susu <28025920@qq.com>
2016-05-04 12:31:14 +0800

    优化了性能，将不需要显示的view不渲染出来，节约内存

--- CustomTabBar/CustomTabBar/ViewController.m
+++ CustomTabBar/CustomTabBar/ViewController.m
@@ -12,6 +12,12 @@
 #import "ThirdViewController.h"
 @interface ViewController ()
 
+//记录当前的控制器View，进入下一个控制器View时移除，节约资源
+@property(nonatomic,strong)UIView* showView;
+
+//一个专门用来做转场动画的View
+@property(nonatomic,strong)UIView* transitionView;
+
 @end
 
 @implementation ViewController
@@ -19,19 +25,35 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    self.transitionView=[[UIView alloc]initWithFrame:CGRectMake(0, 64, self.view.bounds.size.width, self.view.bounds.size.height-64)];
+    
+    [self.view addSubview:self.transitionView];
+    
+    //只有变成子控制器才能接收事件处理
     [self addChildViewController:[[FirstViewController alloc]init]];
     [self addChildViewController:[[SecondTableViewController alloc]init]];
     [self addChildViewController:[[ThirdViewController alloc]init]];
+    
 }
 - (IBAction)buttonClicked:(UIButton *)sender {
     
+    [self.showView removeFromSuperview];
+    
     UIViewController* vc=self.childViewControllers[sender.tag];
     
-    vc.view.frame=CGRectMake(0, 64, self.view.bounds.size.width, self.view.bounds.size.height-64);
+    vc.view.frame=self.transitionView.bounds;
     
-    [self.view addSubview:vc.view];
+    [self.transitionView addSubview:vc.view];
     
+    self.showView=vc.view;
     
+    CATransition* anim=[[CATransition alloc]init];
+    
+    anim.type=@"cube";
+    
+    anim.duration=0.8;
+    
+    [self.transitionView.layer addAnimation:anim forKey:nil];
 }
 
 
